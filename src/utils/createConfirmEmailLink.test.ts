02d39503ae17550a298e329e11ee1549c6067358
Max Li <maxlivinci@gmail.com>
2018-09-10 08:21:27 +0800

    refactoring

--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -17,28 +17,20 @@ beforeAll(async () => {
   userId = user._id;
 });
 
-describe("test createConfirmEmailLink", () => {
-  test("Make sure it confirms user and clears key in redis", async () => {
-    const url = await createConfirmEmailLink(
-      process.env.TEST_HOST as string,
-      userId,
-      redis
-    );
+test("Make sure it confirms user and clears key in redis", async () => {
+  const url = await createConfirmEmailLink(
+    process.env.TEST_HOST as string,
+    userId,
+    redis
+  );
 
-    const response = await fetch(url);
-    const text = await response.text();
-    expect(text).toEqual("ok");
-    const user = await User.findOne({ _id: userId });
-    expect((user as IUserModel).confirmed).toBeTruthy();
-    const chunks = url.split("/");
-    const key = chunks[chunks.length - 1];
-    const value = await redis.get(key);
-    expect(value).toBeNull();
-  });
-
-  test("sends invalid back if bad id sent", async () => {
-    const response = await fetch(`${process.env.TEST_HOST}/confirm/12083`);
-    const text = await response.text();
-    expect(text).toEqual("invalid");
-  });
+  const response = await fetch(url);
+  const text = await response.text();
+  expect(text).toEqual("ok");
+  const user = await User.findOne({ _id: userId });
+  expect((user as IUserModel).confirmed).toBeTruthy();
+  const chunks = url.split("/");
+  const key = chunks[chunks.length - 1];
+  const value = await redis.get(key);
+  expect(value).toBeNull();
 });
